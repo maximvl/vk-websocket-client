bef98bb99f8fe114e82840bda334cb399c802a4e
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-05-01 14:09:06 +0200

    Update settings env

--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ lasqa_channel = "channel-chat:8845069"
 roadhouse_channel = "channel-chat:6367818"
 zhmil_channel = "channel-chat:6639759"
 
-vk_channel = os.getenv("VK_CHANNEL", lasqa_channel)
+vk_channel = os.environ.get("VK_CHANNEL", lasqa_channel)
 
-worker_idle_timeout_seconds = int(os.getenv("IDLE_TIMEOUT_MIN", "15")) * 60
-randomize_votes = os.getenv("RANDOMIZE_VOTES", "false") == "true"
+worker_idle_timeout_seconds = int(os.environ.get("IDLE_TIMEOUT_MIN", "15")) * 60
+randomize_votes = os.environ.get("RANDOMIZE_VOTES", "false") == "true"
 
-zeromq_address = os.getenv("RPC_ADDRESS", "tcp://127.0.0.1:4242")
+zeromq_address = os.environ.get("ZMQ_ADDRESS", "tcp://127.0.0.1:4242")
